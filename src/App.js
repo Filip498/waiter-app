5c189aed6d18d6ea53ed7c852b1a2ef6ae7c34d4
Filip Cheliński <filipchel20@gmail.com>
2022-11-13 19:05:22 -0200

    Add all files to project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import {fetchTables, getAllTables} from './redux/tablesRedux';
+import {useDispatch, useSelector} from 'react-redux';
+import {useEffect} from 'react';
+import {Routes, Route, Navigate} from 'react-router-dom';
+import Table from './components/pages/Table/Table';
+import TablesList from './components/pages/TablesList/TablesList';
+import NotFound from './components/pages/NotFound/NotFound';
+import {Container} from 'react-bootstrap';
+import Header from './components/views/Header/Header';
+import Footer from './components/views/Footer/Footer';
+import PageLoader from './components/common/PageLoader/PageLoader';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  const dispatch = useDispatch();
+  useEffect(() => dispatch(fetchTables()), [dispatch]);
+  const tables = useSelector(state => getAllTables(state));
+
+  if (tables.length === 0) return <PageLoader info="Loading tables data" />;
+  else
+    return (
+      <div className="App">
+        <Container>
+          <Header />
+
+          <Routes>
+            <Route path="/" element={<TablesList />} />
+            {tables.map(table => {
+              return <Route key={table.id} path={'/table/' + table.id} element={<Table id={table.id} />} />;
+            })}
+            <Route path="/table/*" element={<Navigate to="/" />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+          <Footer />
+        </Container>
+      </div>
+    );
+};
 
 export default App;
---
+++ src/redux/store.js
@@ -0,0 +1,25 @@
+import {createStore, combineReducers, applyMiddleware, compose} from 'redux';
+import thunk from 'redux-thunk';
+import initialState from './initialState';
+import tablesReducer from './tablesRedux';
+import statusesReducer from './statusesRedux';
+
+const subreducers = {
+  tables: tablesReducer,
+  statuses: statusesReducer,
+};
+
+const reducer = combineReducers(subreducers);
+
+const store = createStore(
+    //referencja do funkcji odpowiedzialnej za modyfikacje magazynu
+    reducer,
+    //wskazanie danych startowych magazynu
+    initialState,
+    compose(
+      applyMiddleware(thunk),
+      //wpis do wtyczki w chrome
+      window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+  );
+  export default store;
